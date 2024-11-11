749bda0b96190ad9c735b8e60453cc467097ba4f
bukan-tapio <pio.tapio90@gmail.com>
2024-11-11 09:57:29 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+	addNoteHandler,
+	getAllNotesHandler,
+	getNoteIdHandler,
+	editNoteByHandler,
+	deleteNoteById,
+} = require('./handler');
+
+const routes = [
+	{
+		method: 'POST',
+		path: '/notes',
+		handler: addNoteHandler,
+	},
+	{
+		method: 'GET',
+		path: '/notes',
+		handler: getAllNotesHandler,
+	},
+	{
+		method: 'GET',
+		path: '/notes/{id}',
+		handler: getNoteIdHandler,
+	},
+	{
+		method: 'PUT',
+		path: '/notes/{id}',
+		handler: editNoteByHandler,
+	},
+	{
+		method: 'DELETE',
+		path: '/notes/{id}',
+		handler: deleteNoteById,
+	},
+];
+
+module.exports = routes;
