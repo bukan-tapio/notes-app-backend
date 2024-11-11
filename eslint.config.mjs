836eec1e94364fe0595388ed09ba9a59eb9822be
bukan-tapio <pio.tapio90@gmail.com>
2024-11-11 11:57:44 +0700

    giving change for host and package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import daStyle from "eslint-config-dicodingacademy"
-
+import daStyle from "eslint-config-dicodingacademy";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  daStyle,
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default [daStyle, { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } }, { languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended];
